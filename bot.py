65e0a528ed574b92858c76a68d54405a7f62f165
Lorenzo <lorenzovasi@live.com>
2022-11-14 20:19:54 +0100

    Add comments into bot.py; Repair some little (very little) problems

--- bot.py
+++ bot.py
@@ -1,91 +1,79 @@
 import interactions # For command on DS
 import os # Required for dotenv
 from dotenv import load_dotenv # Load library dotenv
-from requests import get
-import json
+from requests import get # For curl request
+import json # For use HA Api
+import logging # For logging
 
-#connection and data pull from Home assistant
+load_dotenv() # Load .env file
 
-api_url = os.getenv('HOMEASSISTANT_URL')  #take HA url from .env file, if the bot and HA has the same host url is something like localhost:8123
-tokenHA = os.getenv('HOMEASSISTANT_TOKEN') # take HA token from .env file
+# Take variable from .env
+api_url = os.getenv('HOMEASSISTANT_URL')  # Take HA url from .env file, if the bot and HA has the same host url is something like localhost:8123
+tokenHA = os.getenv('HOMEASSISTANT_TOKEN') # Take HA Token from .env file
+TOKEN = os.getenv('DISCORD_TOKEN') # Take Discord TOKEN from /env file
 
-headers = { # haders for api access 
+headers = { # Headers for API Access 
     "Authorization": "Bearer " + tokenHA,
     "content-type": "application/json",
 }
 
-# the following part probably can be better
+bot = interactions.Client(token=TOKEN) # Set TOKEN client
 
-# MC server status
+# -----------------------------------
+# Function used in commands
+
+# MC Server Status
 def getMCServerStatus():
-    MinecraftServerStatusPull = get(api_url + "states/binary_sensor.minecraft_server_status", headers=headers, )
-    MinecraftServerStatusJson = json.loads(MinecraftServerStatusPull.text)
-    MinecraftServerStatus = MinecraftServerStatusJson["state"] # variable to use
+    MinecraftServerStatusPull = get(api_url + "api/states/binary_sensor.minecraft_server_status", headers=headers, ) # Curl Request for take json file
+    MinecraftServerStatusJson = json.loads(MinecraftServerStatusPull.text) # Json to object
+    MinecraftServerStatus = MinecraftServerStatusJson["state"] # Variable with data
     return MinecraftServerStatus
 
-# MC server latency
-def getMCServerLatency():
-    MinecraftServerLatencyPull = get(api_url + "states/binary_sensor.minecraft_latency_time", headers=headers, )
-    MinecraftServerLatencyJson = json.loads(MinecraftServerLatencyPull.text)
-    MinecraftServerLatency = MinecraftServerLatencyJson["state"] # variable to use
-    return MinecraftServerLatency
-
-# MC server max player number
+# MC Server max_player number
 def getMCServerPlayerMax():
-    MinecraftServerPlayerMaxPull = get(api_url + "states/binary_sensor.minecraft_server_players_max", headers=headers, )
-    MinecraftServerPlayerMaxJson = json.loads(MinecraftServerPlayerMaxPull.text)
-    MinecraftServerPlayerMax = MinecraftServerPlayerMaxJson["state"] # variable to use
+    MinecraftServerPlayerMaxPull = get(api_url + "api/states/sensor.minecraft_server_players_max", headers=headers, ) # Curl Request for take json file
+    MinecraftServerPlayerMaxJson = json.loads(MinecraftServerPlayerMaxPull.text) # Json to object
+    MinecraftServerPlayerMax = MinecraftServerPlayerMaxJson["state"] # Variable with data
     return MinecraftServerPlayerMax
 
-# MC server online players number
+# MC Server online_players number
 def getMCServerPlayerOnline():
-    MinecraftServerPlayerOnlinePull = get(api_url + "states/binary_sensor.minecraft_server_players_online", headers=headers, )
-    MinecraftServerPlayerOnlineJson = json.loads(MinecraftServerPlayerOnlinePull.text)
-    MinecraftServerPlayerOnline = MinecraftServerPlayerOnlineJson["state"] # variable to use
+    MinecraftServerPlayerOnlinePull = get(api_url + "api/states/sensor.minecraft_server_players_online", headers=headers, ) # Curl Request for take json file
+    MinecraftServerPlayerOnlineJson = json.loads(MinecraftServerPlayerOnlinePull.text) # Json to object
+    MinecraftServerPlayerOnline = MinecraftServerPlayerOnlineJson["state"] # Variable with data
     return MinecraftServerPlayerOnline
-
-# MC server protocol version
-def getMCServerProtocolVersion():
-    MinecraftServerProtocolVersionPull = get(api_url + "states/binary_sensor.minecraft_server_protocol_version", headers=headers, )
-    MinecraftServerProtocolVersionJson = json.loads(MinecraftServerProtocolVersionPull.text)
-    MinecraftServerProtocolVersion = MinecraftServerProtocolVersionJson["state"] # variable to use
-    return MinecraftServerProtocolVersion
 
 # MC server version
 def getMCServerVersion():
-    MinecraftServerVersionPull = get(api_url + "states/binary_sensor.minecraft_server_server_version", headers=headers, )
-    MinecraftServerVersionJson = json.loads(MinecraftServerVersionPull.text)
-    MinecraftServerVersion = MinecraftServerVersionJson["state"] # variable to use
+    MinecraftServerVersionPull = get(api_url + "api/states/sensor.minecraft_server_version", headers=headers, ) # Curl Request for take json file
+    MinecraftServerVersionJson = json.loads(MinecraftServerVersionPull.text) # Json to object
+    MinecraftServerVersion = MinecraftServerVersionJson["state"] # Variable with data
     return MinecraftServerVersion
 
+# ------------------------------------
 
-load_dotenv() # Load .env file
-TOKEN = os.getenv('DISCORD_TOKEN') # Take variable from env file
 
-bot = interactions.Client(token=TOKEN) # Set TOKEN client
-
-@bot.command( # Command test
+@bot.command( # Command Test Event
     name="test",
     description="Testing command",
 )
-async def my_first_command(ctx: interactions.CommandContext):
-    await ctx.send("Finalmente!")
+async def my_first_command(ctx: interactions.CommandContext): # Command Test Function of previous event
+    await ctx.send("Hi there!") # Send message to DS
 
-@bot.command( #minecraft server info command
-    name="InfoServerMinecraft",
+
+
+@bot.command( # Minecraft Server Status
+    name="infoserverminecraft",
     description="Ottieni informazioni sul server",
 )
 async def serverinfo(ctx: interactions.CommandContext):
     
     MinecraftServerStatus = getMCServerStatus()
-    MinecraftServerLatency = getMCServerLatency()
     MinecraftServerVersion = getMCServerVersion()
     MinecraftServerPlayerMax = getMCServerPlayerMax()
     MinecraftServerPlayerOnline = getMCServerPlayerOnline()
-    MinecraftServerProtocolVersion = getMCServerProtocolVersion()
 
-    message = "Server status: " + getMCServerStatus() + "\n" + "Latenza: " + getMCServerLatency() + "\n" + "Versione server: " + getMCServerVersion() + "\n" + "giocatori massimi: " + getMCServerPlayerMax() + "\n" + "Numero giocatori online" + getMCServerPlayerOnline() + "\n" + "Versione protocollo: " + MinecraftServerProtocolVersion
-    
+    message = "Server Minecraft v." + MinecraftServerVersion + ": stato " + MinecraftServerStatus + "\nNumero Giocatori: " + MinecraftServerPlayerOnline + "/" + MinecraftServerPlayerMax
     
     await ctx.send(message)
 
